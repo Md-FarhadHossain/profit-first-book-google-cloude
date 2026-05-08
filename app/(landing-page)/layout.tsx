6b06d3e5178103a67b66eef87a0853f3fc4d4443
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2026-05-08 18:36:53 +0600

    google tag added

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -41,10 +41,33 @@ export default function RootLayout({
       <head>
          {/* FontAwesome - Loaded in head for immediate availability to prevent flickering, but widely cached */}
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+         {/* Google Tag Manager */}
+         <script
+           dangerouslySetInnerHTML={{
+             __html: `
+               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+               })(window,document,'script','dataLayer','GTM-PZ3J5T33');
+             `,
+           }}
+         />
+         {/* End Google Tag Manager */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${hindSiliguri.variable} antialiased ${myFont.className}`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PZ3J5T33"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <FacebookPixel />
 
         {/* Microsoft Clarity Script - Added Here */}
